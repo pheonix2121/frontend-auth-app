06ff26c24f1d0487c83d42c45f811717d03eda35
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-06-03 15:16:38 +0530

    auth app task19

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,13 +1,19 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import AuthContext from '../../store/auth-context';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+  const navigate= useNavigate()
   const authCtx = useContext(AuthContext);
 
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler=()=>{
+    authCtx.logout();
+    navigate("/auth")
+  }
 
   return (
     <header className={classes.header}>
@@ -28,7 +34,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
