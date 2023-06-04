142c27904e10223764c6bfe1b1f3603b0421714e
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-06-04 11:50:41 +0530

    auth app task20

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import AuthContext from './store/auth-context';
+import { useContext, useEffect } from "react";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authCtx.isLoggedIn) {
+      navigate("/auth", { replace: true });
+    }
+  }, [authCtx.isLoggedIn, navigate]);
+
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<UserProfile />} />
+        {!authCtx.isLoggedIn && (
+          <Route path="/auth" element={<AuthPage />} />
+        )}
+        <Route path="/profile" element={authCtx.isLoggedIn ? <UserProfile /> : null} />
+        <Route path="*" element={authCtx.isLoggedIn ? null : <Navigate to="/auth" replace />} />
       </Routes>
     </Layout>
   );
